564dcc7da4943515ed2d9fd5447372b904110d36
Teus-Scripts <mateusboaventura779@gmail.com>
2024-11-11 00:27:10 -0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,14 +1,1 @@
--- Configuração
-local mensagem = ".GG/TEUSCOMMUNITY"
-local tempo = 30 -- segundos
 
--- Função para enviar mensagem
-local function enviarMensagem()
-    game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(mensagem, "All")
-end
-
--- Loop para enviar mensagem a cada 30 segundos
-while true do
-    wait(tempo)
-    enviarMensagem()
-end
